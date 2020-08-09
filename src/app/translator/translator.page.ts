99d3c76d72dacbcd5b3c65c53def51ae1f454f6b
kapis18 <kapis1881@gmail.com>
2020-08-09 23:20:39 +0200

    Added loadings and two new languages

--- src/app/translator/translator.page.ts
+++ src/app/translator/translator.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 import { MLKitTranslate } from '@ionic-native/mlkit-translate/ngx';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-translator',
@@ -14,12 +15,15 @@ export class TranslatorPage implements OnInit {
   sourceLang: string = "English";
   targetLang: string = "Polish";
   map: Map<string, string> = new Map<string, string>();
-  constructor(private route: ActivatedRoute, private insomnia: Insomnia, private mlkitTranslate: MLKitTranslate) {
+  currentLoading;
+  constructor(private route: ActivatedRoute, private insomnia: Insomnia, private mlkitTranslate: MLKitTranslate, public loadingController: LoadingController) {
     this.map.set("polish", "pl");
     this.map.set("english", "en");
     this.map.set("russian", "ru");
     this.map.set("german", "de");
-   }
+    this.map.set("french", "fr");
+    this.map.set("spanish", "es");
+  }
 
   ngOnInit() {
   }
@@ -34,8 +38,66 @@ export class TranslatorPage implements OnInit {
     this.translateSource();
   }
   translateSource() {
+    this.areModelsDownloaded();
     this.mlkitTranslate.translate(this.text, this.map.get(this.targetLang.toLowerCase()), this.map.get(this.sourceLang.toLowerCase())).then(translatedText => {
       this.translated = translatedText;
     })
+  }
+  sourceChange($event) {
+    this.translateSource()
+  }
+  targetChange($event) {
+    this.translateSource()
+  }
+  async areModelsDownloaded() {
+    const loading = await this.loadingController.create({ message: 'Loading...' });
+    loading.present().then(() => {
+      this.mlkitTranslate.getDownloadedModels().then(availableModels => {
+        let areAvailable;
+        if (availableModels.some(item => (item.code === this.map.get(this.targetLang.toLowerCase())))) {
+          if (availableModels.some(item => (item.code === this.map.get(this.sourceLang.toLowerCase())))) {
+            areAvailable = 3;
+          }
+          else {
+            areAvailable = 1;
+          }
+        }
+        else if (availableModels.some(item => (item.code === this.map.get(this.sourceLang.toLowerCase())))) {
+          areAvailable = 2;
+        }
+        else {
+          areAvailable = 0;
+        }
+        return areAvailable;
+      }).then(async (areAvailable) => {
+        if (areAvailable != 3) {
+          loading.dismiss().then(async () => {
+            const downloading = await this.loadingController.create({ message: 'Downloading models...' });
+            downloading.present().then(() => {
+              if (areAvailable === 1) {
+                this.mlkitTranslate.downloadModel(this.map.get(this.sourceLang.toLowerCase())).then(() => {
+                  downloading.dismiss();
+                })
+              }
+              else if (areAvailable === 2) {
+                this.mlkitTranslate.downloadModel(this.map.get(this.targetLang.toLowerCase())).then(() => {
+                  downloading.dismiss();
+                })
+              }
+              else if (areAvailable === 0) {
+                this.mlkitTranslate.downloadModel(this.map.get(this.targetLang.toLowerCase())).then(() => {
+                  this.mlkitTranslate.downloadModel(this.map.get(this.targetLang.toLowerCase())).then(() => {
+                    downloading.dismiss();
+                  })
+                })
+              }
+            })
+          });
+        }
+        else {
+          loading.dismiss();
+        }
+      });
+    });
   }
 }
