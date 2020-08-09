815415bcb6394a6df7edda7d9ec4dbacf4635e3f
kapis18 <kapis1881@gmail.com>
2020-08-09 18:16:08 +0200

    Added text translation between 4 languages

--- src/app/translator/translator.page.ts
+++ src/app/translator/translator.page.ts
@@ -10,21 +10,32 @@ import { MLKitTranslate } from '@ionic-native/mlkit-translate/ngx';
 })
 export class TranslatorPage implements OnInit {
   text: string;
-  translated:string = " ";
-  constructor(private route: ActivatedRoute, private insomnia: Insomnia, private mlkitTranslate: MLKitTranslate) { }
+  translated: string = " ";
+  sourceLang: string = "English";
+  targetLang: string = "Polish";
+  map: Map<string, string> = new Map<string, string>();
+  constructor(private route: ActivatedRoute, private insomnia: Insomnia, private mlkitTranslate: MLKitTranslate) {
+    this.map.set("polish", "pl");
+    this.map.set("english", "en");
+    this.map.set("russian", "ru");
+    this.map.set("german", "de");
+   }
 
   ngOnInit() {
   }
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.insomnia.allowSleepAgain()
-    .then(
-      () => console.log('success'),
-      () => console.log('error')
-    );
+      .then(
+        () => console.log('success'),
+        () => console.log('error')
+      );
     let json = JSON.parse(this.route.snapshot.paramMap.get('text'));
     this.text = json.blocks.blocktext.join(" ");
-    this.mlkitTranslate.translate(this.text, "pl", "en").then(translatedText=>{
+    this.translateSource();
+  }
+  translateSource() {
+    this.mlkitTranslate.translate(this.text, this.map.get(this.targetLang.toLowerCase()), this.map.get(this.sourceLang.toLowerCase())).then(translatedText => {
       this.translated = translatedText;
-  })
+    })
   }
 }
